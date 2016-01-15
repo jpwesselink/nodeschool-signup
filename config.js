8cfe033eeb66407792737133ea07303ce40fda13
J.P. Wesselink <J.P.Wesselink@ing.nl>
2016-01-15 11:52:20 +0100

    Added whitespace

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'nodeschool-haarlem.',
+  community: process.env.COMMUNITY_NAME || 'nodeschool-haarlem',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'nodeschool-haarlem.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
   // If you generate the token in owner user, missing_scope error will be occurred.
+  //
   //
   // You can test your token via curl:
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
